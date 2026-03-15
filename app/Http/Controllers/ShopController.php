333c93de1967de204cb163339e94fc9f91a11a71
ibrzica1 <igorbrzica1@gmail.com>
2026-03-15 15:29:27 +0100

    Optimize add Product function

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Validation\Rules\File;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SaveProductRequest;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Repositories\ProductRepository;
@@ -28,19 +29,8 @@ class ShopController extends Controller
         return view("addProduct");
     }
 
-    public function addProduct(Request $request)
+    public function addProduct(SaveProductRequest $request)
     {
-        $request->validate([
-            "name" => "required|string|min:2|max:50|unique:products,name",
-            "description" => "required|string|min:2|max:150",
-            "amount" => "required|int",
-            "price" => "required|between:0,99.99",
-            "image" => "required",
-            File::types(['jpg','jpeg','png','gif'])
-            ->min(1024)
-            ->max(12 * 1024),
-        ]);
-
         $this->productRepo->createNew($request);
 
         return redirect()->route('allProducts');
