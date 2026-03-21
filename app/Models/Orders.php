88de9db404bc7e2e19cfa3fdb618712604d86098
ibrzica1 <igorbrzica1@gmail.com>
2026-03-21 16:14:30 +0100

    Create orderTotalPrice function

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -2,11 +2,27 @@
 
 namespace App\Models;
 
+use App\Repositories\ProductRepository;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Session;
 
 class Orders extends Model
 {
     protected $fillable = [
         'user_id','status','price',
     ];
+
+    public static function orderTotalPrice()
+    {
+        $total = 0;
+        $products = Session::get('products');
+        $productRepo = new ProductRepository();
+
+        foreach($products as $product)
+        {
+            $total += $productRepo->calculateTotal($product['product']['id'],$product['amount']);
+        }
+        
+        return $total;
+    }
 }
