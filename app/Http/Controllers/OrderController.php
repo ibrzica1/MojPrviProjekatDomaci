5c051bbf0eca5ac7ca88ce047f7c13298eaf4bc9
ibrzica1 <igorbrzica1@gmail.com>
2026-03-21 16:55:47 +0100

    Create addOrder in OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\OrderItems;
 use App\Models\Orders;
+use App\Repositories\OrderItemsRepository;
 use App\Repositories\OrderRepository;
 use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class OrderController extends Controller
@@ -22,6 +25,7 @@ class OrderController extends Controller
     public function addOrder()
     {
         $productRepo = new ProductRepository();
+        $orderItemRepo = new OrderItemsRepository();
         $orderItems = Session::get('products');
         
         foreach($orderItems as $orderItem)
@@ -30,14 +34,22 @@ class OrderController extends Controller
                 return redirect()->back()->with('error',
                 `Product: {$orderItem['product']['name']} doesnt have enough items in stock`);
             }
+            $productRepo->reduceAmount($orderItem['product']['id'],$orderItem['amount']);
         }
 
         $totalOrderPrice = Orders::orderTotalPrice();
         $userId = Auth::id();
         $status = "ordered";
-
-        $this->orderRepo->createOrder($userId,$status,$totalOrderPrice);
+        $orderId = $this->orderRepo->createOrder($userId,$status,$totalOrderPrice);
         
+        foreach($orderItems as $orderItem)
+        {
+            $totalItemPrice = OrderItems::calculateItemTotal($orderItem['product']['price'],$orderItem['amount']);
+            $orderItemRepo->addOrderItem($orderId,$orderItem['amount'],$orderItem['product']['id'],$totalItemPrice);
+        }
+        Session::forget('products');
+
+        return redirect()->route('home');
     }
 
 }
